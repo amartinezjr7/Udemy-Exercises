67bec79225eca164adb4300db1a3099c55c04dec
amartinezjr7 <94876426+amartinezjr7@users.noreply.github.com>
2023-01-28 15:50:31 -0800

    java stuff

---
+++ Master OOD-Java/SingleResponsibility/EmployeeDAO.java
@@ -0,0 +1,6 @@
+public class EmployeeDAO {
+    public void saveEmployee(Employee employee){
+        DatabaseConnectionManager connectionManager = DatabaseConnectionManager.getManagerInstance();
+    }
+    public void deleteEmployee(Employee employee)
+}
